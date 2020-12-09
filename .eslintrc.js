90ec0cc2a18b574f3dc165cf6f3873aaa08dc63b
Thibaud Duthoit <contact@thibaud-duthoit.fr>
2020-12-09 20:56:35 +0100

    Rule react/react-in-jsx-scope as off

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     },
   },
   rules: {
+    "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
     "react/no-unescaped-entities": "off",
     "no-console": "warn",
