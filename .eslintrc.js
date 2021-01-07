818ee58afb210ed1b8912e1749bea1312eff91ac
Thibaud Duthoit <contact@thibaud-duthoit.fr>
2021-01-07 21:29:16 +0100

    Reorders extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
     "prettier",
     "prettier/@typescript-eslint",
   ],
